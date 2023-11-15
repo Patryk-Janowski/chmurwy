ea65d98e5cfe01ac52596428303eb390b837a06b
Patryk Janowski <pkjanowski@gmail.com>
2023-11-15 20:00:53 +0100

    _

--- README.md
+++
@@ -1,2 +0,0 @@
-# chmurwy
-Projekt Chmury
--- import_eb_env/eb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_elastic_beanstalk_environment" "hehe" {
-  application = "best-vulpy-hr7lubkw"
-  name = "best-vulpy-hr7lubkw-env"
-}
-
-import {
-  id = "e-rghtse7qiv"
-  to = aws_elastic_beanstalk_environment.hehe
-}
